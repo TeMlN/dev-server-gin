915039cd99e5c88234d01b5b0c62a00c0e541aad
taemin <youman7953@naver.com>
2021-04-05 20:46:11 +0900

    [db info config]

--- config/config.go
+++ config/config.go
@@ -1,6 +1,42 @@
 package config
 
-type Tag struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+)
+
+//db config
+var Conf Config
+
+type Config struct {
+	APP      app
+	Database database
+	Format   format
+}
+
+type database struct {
+	User     string
+	Password string
+	Server   string
+	Port     string
+	Engine   string
+	Database string
+}
+
+type format struct {
+	Format string
+}
+
+type app struct {
+	Name string `toml:"name"`
+}
+
+func StartConfig() {
+	_, err := toml.DecodeFile("./config.toml", &Conf)
+	err != nil {
+		panic(err)
+	}
+	fmt.Printf("%#v\n", Conf)
+
 }
